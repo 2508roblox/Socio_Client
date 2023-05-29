ea4ccd7c99afef8ecddf3fefafa22b3bfe6fccb4
2508roblox <trangiangzxc@gmail.com>
2023-05-29 12:14:14 +0700

    fix-base-router

--- src/index.js
+++ src/index.js
@@ -5,21 +5,15 @@ import App from './App';
 //redux
 import store from './reducers/store'
 import { Provider } from 'react-redux'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-const router = createBrowserRouter([
-  {
-    path: "*",
-    element: <App />
-  },
-]);
+import { BrowserRouter } from 'react-router-dom';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={router} />
+      <BrowserRouter basename="/Socio_Client">
+        <App />
+      </BrowserRouter>,
     </Provider>
 
   </React.StrictMode>
